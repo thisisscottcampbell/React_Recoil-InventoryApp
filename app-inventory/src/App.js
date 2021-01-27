6f4279a176bb84bc01e893f98e782c8c617cbbce
Scott Campbell <stc9486@gmail.com>
2021-01-27 15:15:07 -0500

    set state

---
+++ app-inventory/src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+	RecoilRoot,
+	atom,
+	selector,
+	useRecoilState,
+	useRecoilValue,
+} from 'recoil';
+
+const inventory = {
+	a: { name: 'Yerba Mate', price: 10 },
+	b: { name: 'Coffee', price: 15 },
+	c: { name: 'Tea', price: 7 },
+};
+
+const destinations = {
+	US: 25,
+	CA: 35,
+	CO: 45,
+};
+
+const cartState = atom({
+	key: 'cartState',
+	default: {},
+});
+
+const App = () => {
+	return (
+		<RecoilRoot>
+			<AvailableItems />
+			<Cart />
+		</RecoilRoot>
+	);
+};
+
+const AvailableItems = () => {
+	const [cart, setCart] = useRecoilState(cartState);
+	return (
+		<div>
+			<h2>Available Items</h2>
+			<pre>{JSON.stringify(cart, null, 2)}</pre>
+			<ul>
+				{Object.entries(inventory).map(([id, { name, price }]) => (
+					<li key={id}>
+						{name} @ ${price.toFixed(2)}
+						<button
+							onClick={() => {
+								setCart({ ...cart, [id]: (cart[id] || 0) + 1 });
+							}}
+						>
+							add item
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+const Cart = () => {
+	return (
+		<div>
+			<h2>Cart</h2>
+		</div>
+	);
+};
+export default App;
